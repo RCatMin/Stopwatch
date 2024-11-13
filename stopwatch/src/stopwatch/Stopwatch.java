43a455bcaad1f78391459edf574e195c4fc486ce
RCatMin <razzcat_min@naver.com>
2024-11-13 13:51:04 +0900

    Create Stopwatch Class

---
+++ stopwatch/src/stopwatch/Stopwatch.java
@@ -0,0 +1,18 @@
+package stopwatch;
+
+import java.util.Scanner;
+
+public class Stopwatch {
+	
+	private Scanner scan = new Scanner (System.in);
+	
+	private boolean isRun = true;
+	
+	public void run () {
+		
+		while (isRun) {
+			
+		}
+	}
+	
+}
